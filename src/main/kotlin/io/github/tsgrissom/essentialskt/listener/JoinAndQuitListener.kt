256876fa2914eb57d9d654543688673f4ca7d6ac
tsgrissom <tylergrissom@me.com>
2023-09-10 12:53:06 -0500

    Improved null safety of join and quit messages

--- src/main/kotlin/io/github/tsgrissom/essentialskt/listener/JoinAndQuitListener.kt
+++ src/main/kotlin/io/github/tsgrissom/essentialskt/listener/JoinAndQuitListener.kt
@@ -15,19 +15,20 @@ class JoinAndQuitListener : Listener {
         return plugin.config ?: throw IllegalStateException("config is null")
     }
 
+    private fun getJoinMessage() = getConfiguration().getString("Messages.JoinEvent", "&a&l+ &e%player% has joined the server")!!
+    private fun getQuitMessage() = getConfiguration().getString("Messages.QuitEvent", "&c&l- &e%player% has left the server")!!
+
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
-        e.joinMessage = getConfiguration()
-            .getString("Messages.JoinEvent")
-            ?.translateColor()
-            ?.replace("%player%", e.player.displayName)
+        e.joinMessage = getJoinMessage()
+            .translateColor()
+            .replace("%player%", e.player.displayName)
     }
 
     @EventHandler
     fun onQuit(e: PlayerQuitEvent) {
-        e.quitMessage = getConfiguration()
-            .getString("Messages.QuitEvent")
-            ?.translateColor()
-            ?.replace("%player%", e.player.displayName)
+        e.quitMessage = getQuitMessage()
+            .translateColor()
+            .replace("%player%", e.player.displayName)
     }
 }
