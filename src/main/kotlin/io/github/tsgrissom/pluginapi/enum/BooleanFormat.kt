a5d13c3c31527eff337186fc2403510b67e918e5
Tyler Grissom <tylergrissom@me.com>
2023-10-30 14:48:01 -0500

    Documented BooleanFormat enum

--- src/main/kotlin/io/github/tsgrissom/pluginapi/enum/BooleanFormat.kt
+++ src/main/kotlin/io/github/tsgrissom/pluginapi/enum/BooleanFormat.kt
@@ -1,9 +1,32 @@
 package io.github.tsgrissom.pluginapi.enum
 
+/**
+ * Enumeration of the types of BooleanFormats for the Boolean#fmt extension method. Provides a true and a false String.
+ */
 enum class BooleanFormat(val trueStr: String, val falseStr: String) {
+
+    /**
+     * true->"enable" or false->"disable"
+     */
     ENABLE_DISABLE("enable", "disable"),
+
+    /**
+     * true->"enabled" or false->"disabled"
+     */
     ENABLED_DISABLED("enabled", "disabled"),
+
+    /**
+     * true->"true" or false->"false"
+     */
     TRUE_FALSE("true", "false"),
+
+    /**
+     * true->"on" or false->"off"
+     */
     ON_OFF("on", "off"),
+
+    /**
+     * true->"yes" or false->"no"
+     */
     YES_NO("yes", "no");
 }
